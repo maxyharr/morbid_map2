3a360465a55c584f4f548aef5872ee821d7b26ef
Max Harris <mjharris@colorado.edu>
2015-04-30 12:23:23 -0600

    finished for presentation

---
+++ public/angular-datamaps.js
@@ -0,0 +1,147 @@
+'use strict';
+angular.module('datamaps', []);
+'use strict';
+angular.module('datamaps').directive('datamap', [
+  '$compile',
+  function ($compile) {
+    return {
+      restrict: 'EA',
+      scope: {
+        data: '=',
+        options: '=',
+        colors: '=?',
+        type: '@?',
+        onClick: '&?',
+        fills: '=?'
+      },
+      link: function (scope, element, attrs) {
+        // Generate base map options
+        function mapOptions() {
+          return {
+            element: element[0].children[0],
+            scope: scope.type === 'usa' || scope.type === 'world' ? scope.type : 'usa',
+            height: scope.height,
+            width: scope.width,
+            projection: scope.type === 'world' ? 'mercator' : 'equirectangular',
+            fills: { defaultFill: '#b9b9b9' },
+            data: {},
+            done: function (datamap) {
+              if (angular.isDefined(attrs.onClick)) {
+                datamap.svg.selectAll('.datamaps-subunit').on('click', function (geography) {
+                  scope.onClick()(geography);
+                });
+              }
+            }
+          };
+        }
+        // Extend the mapOptions object with data and fill values
+        function mapData(dst, data) {
+          angular.forEach(data, function (val) {
+            dst.data[val.location] = { fillKey: val.value };
+          });
+          if (angular.isDefined(scope.fills)) {
+            dst.fills = scope.fills;
+          } else if (!scope.options.fillQuartiles) {
+            var fillKeys = [];
+            angular.forEach(data, function (data) {
+              if (fillKeys.indexOf(data.value) === -1) {
+                fillKeys.push(data.value);
+              }
+            });
+            if (angular.isUndefined(scope.colors)) {
+              scope.colors = defaultColors();
+            }
+            angular.forEach(fillKeys, function (key, idx) {
+              dst.fills[key] = scope.colors[idx];
+            });
+          } else {
+          }
+          return dst;
+        }
+        // Generate default colors
+        function defaultColors() {
+          var d3colors = d3.scale.category20(), colors = [];
+          for (var i = 0; i < 20; i++) {
+            colors.push(d3colors(i));
+          }
+          return colors;
+        }
+        scope.mapOptions = mapOptions();
+        scope.api = {
+          refresh: function () {
+            scope.api.updateWithOptions(scope.options, scope.data);
+          },
+          updateWithOptions: function (options, data) {
+            // Clearing
+            scope.api.clearElement();
+            // Exit if options or data are not yet bound
+            if (!angular.isDefined(options) || !scope.data.length) {
+              return;
+            }
+            // Update bounding box
+            scope.width = options.width || 600;
+            scope.height = options.height || scope.width * 0.6;
+            scope.legendHeight = options.legendHeight || 50;
+            scope.mapOptions = mapOptions();
+            // Add data to map redraw
+            if (data[0].values.length) {
+              // update the map element
+              scope.mapOptions = mapData(scope.mapOptions, data[0].values);
+            }
+            scope.mapOptions.geographyConfig = angular.extend({}, options.geographyConfig);
+            scope.map = new Datamap(scope.mapOptions);
+            if (!options) {
+              return;
+            }
+            // set labels
+            if (options.labels) {
+              scope.map.labels({
+                labelColor: options.labelColor ? options.labelColor : '#333333',
+                fontSize: options.labelSize ? options.labelSize : 12
+              });
+            }
+            // set legend
+            if (options.legend) {
+              scope.map.legend();
+            }
+          },
+          updateWithData: function (data) {
+            scope.map.updateChoropleth(data);
+          },
+          clearElement: function () {
+            scope.map = null;
+            element.empty();
+            var mapContainer = $compile('<div style="position: relative; display: block; padding-bottom: {{ legendHeight }}px;"></div>')(scope);
+            element.append(mapContainer);
+          }
+        };
+        // Watching on options, colors changing
+        scope.$watch('[options, colors]', function () {
+          scope.api.refresh();
+        }, true);
+        // Watch data changing. Only refresh if options or data map points have changed
+        scope.$watch('data', function (data, old) {
+          // Return if no data
+          if (!data.length) {
+            return;
+          }
+          if (!old.length || data[0].values.length !== old[0].values.length) {
+            scope.api.refresh();
+          } else {
+            var _data = {};
+            angular.forEach(data[0].values, function (val) {
+              _data[val.location] = { fillKey: val.value };
+              // Set extra fields in case we want to use them in the popup
+              angular.forEach(val, function (prop, key) {
+                if (key !== 'value' && key !== 'location') {
+                  _data[val.location][key] = prop;
+                }
+              });
+            });
+            scope.api.updateWithData(_data);
+          }
+        }, true);
+      }
+    };
+  }
+]);
--- public/core.js
+++ public/core.js
@@ -2,7 +2,6 @@
 var morbidMap = angular.module('morbidMap', []);
 
 function mainController($scope, $http) {
-    $scope.loc = 'usa'
 
     $scope.states = [{ name: 'alabama', abbreviation: 'AL'},
     { name: 'alaska', abbreviation: 'AK'},
@@ -58,15 +57,64 @@ function mainController($scope, $http) {
 
 
     $scope.causes_of_death = {}
+    $scope.max_death_proportion = 0;
+    $scope.max_state_deaths = 0;
     $scope.maincause = "The leading cause of death in each state (1999-2013)"
 
-    // when landing on the page, get all todos and show them
+    $scope.map = new Datamap({
+        element: document.getElementById('map'),
+        scope: 'usa',
+        fills: {
+            VERY_HIGH: '#9b211e',
+            HIGH: '#be413e',
+            MEDIUM: '#e36b68',
+            LOW: '#ff9f9c',
+            VERY_LOW: '#ffc6c4',
+            UNKNOWN: '#eeeeee',
+            defaultFill: '#eeeeee'
+        },
+        data: {
+                    
+        },
+        geographyConfig: {
+            popupTemplate: function(geo, data) {
+                return ['<div class="hoverinfo"><strong>',
+                'State: ' + geo.properties.name + '<br>',
+                'Gender: ' + data.gender + '<br>',
+                'Age Group: ' + data.age_group + '<br>',
+                'Deaths: ' + data.deaths + '<br>',
+                'Population: ' + data.population + '<br>',
+                'Cause of Death: ' + data.cause_of_death + '<br>',
+                'Crude Rate: ' + data.crude_rate,
+                '</strong></div>'].join('');
+            }
+        }
+    });
+    $scope.map.labels()
+
+
     for (var i in $scope.states) {
         var state = $scope.states[i].name
-
         $http.get('/causes_of_death/' + state) // may also want to create a custom query using params - see iphone
             .success(function(data) {
-                $scope.causes_of_death[data.state] = data;
+                if (data) { 
+                    data['fillKey'] = 'UNKNOWN';
+                    $scope.causes_of_death[data.abbreviation.toUpperCase()] = data;
+                    if (data.crude_rate > $scope.max_death_proportion) {
+                        $scope.max_death_proportion = data.crude_rate;
+                        $scope.max_state_proportion = data.state;
+                    }
+                    if (data.deaths > $scope.max_state_deaths) {
+                        $scope.max_state_deaths = data.deaths;
+                        $scope.max_state_deaths_name = data.state;
+                    }
+                    if (data.crude_rate > $scope.max_death_proportion*4/5) { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'VERY_HIGH'; } 
+                    else if (data.crude_rate > $scope.max_death_proportion*3/5) { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'HIGH'; } 
+                    else if (data.crude_rate > $scope.max_death_proportion*2/5) { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'MEDIUM'; } 
+                    else if (data.crude_rate > $scope.max_death_proportion*1/5) { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'LOW'; } 
+                    else { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'VERY_LOW'; } 
+                    $scope.map.updateChoropleth($scope.causes_of_death)
+                }
             })
             .error(function(data) {
                 console.log('Error: ' + data);
@@ -74,25 +122,41 @@ function mainController($scope, $http) {
     }
 
     $scope.refresh = function() {
-        $scope.causes_of_death = {}
-        var searchElement = document.getElementById('search')
-        var genderElement = document.getElementById('gender')  
-        var agegroupElement = document.getElementById('agegroup')
+        $scope.causes_of_death = {};
+        $scope.max_death_proportion = 0;
+        $scope.max_state_deaths = 0;
+        var searchElement = document.getElementById('search');
+        var genderElement = document.getElementById('gender'); 
+        var agegroupElement = document.getElementById('agegroup');
         
-        var search = searchElement.value
-        var gender = genderElement.options[genderElement.selectedIndex].value 
-        var age_group = agegroupElement.options[agegroupElement.selectedIndex].value
+        var search = searchElement.value;
+        var gender = genderElement.options[genderElement.selectedIndex].value;
+        var age_group = agegroupElement.options[agegroupElement.selectedIndex].value;
 
-        if (search !== '') {$scope.maincause = "Showing diseases with highest death tolls containing the word(s) " + "'" + search + "'"}
+        if (search !== '') {$scope.maincause = "Showing diseases with highest death tolls containing the word(s) " + "'" + search + "'"};
 
 
         for (var i in $scope.states) {
-            var state = $scope.states[i].name
-            console.log(search)
+            var state = $scope.states[i].name;
             $http.get('/search/' + search + '/' + gender + '/' + state + '/' + age_group)
                 .success(function(data) {
                     if (data) {
-                        $scope.causes_of_death[data.state] = data;
+                        data['fillKey'] = 'UNKNOWN';
+                        $scope.causes_of_death[data.abbreviation.toUpperCase()] = data;
+                        if (data.crude_rate > $scope.max_death_proportion) {
+                            $scope.max_death_proportion = data.crude_rate;
+                            $scope.max_state_proportion = data.state;
+                        }
+                        if (data.deaths > $scope.max_state_deaths) {
+                            $scope.max_state_deaths = data.deaths;
+                            $scope.max_state_deaths_name = data.state;
+                        }
+                        if (data.crude_rate > $scope.max_death_proportion*4/5) { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'VERY_HIGH'; } 
+                        else if (data.crude_rate > $scope.max_death_proportion*3/5) { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'HIGH'; } 
+                        else if (data.crude_rate > $scope.max_death_proportion*2/5) { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'MEDIUM'; } 
+                        else if (data.crude_rate > $scope.max_death_proportion*1/5) { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'LOW'; } 
+                        else { $scope.causes_of_death[data.abbreviation.toUpperCase()]['fillKey'] = 'VERY_LOW'; } 
+                        $scope.map.updateChoropleth($scope.causes_of_death)
                     }
                 })
                 .error(function(data){  
